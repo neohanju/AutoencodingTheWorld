4be121d999fd05d32b1ef363b66bc3d76e649ad0
Lee JeYeol <egg5562@naver.com>
2017-04-09 18:14:26 +0900

    separate preprocessing and add coments

--- data_generation/generate_sample.py
+++ data_generation/generate_sample.py
@@ -5,15 +5,13 @@ import numpy
 import shutil
 import torch
 
-
 FFMPEG_BIN = "ffmpeg"
 DATASET_PATH = '/home/leejeyeol/Datasets'
 
-
 number_of_images_in_dataset_all = 0
 
-target_rows = 113
-target_cols = 113
+target_rows = 227
+target_cols = 227
 
 target_length = 10
 frame_stride = 2
@@ -23,7 +21,7 @@ dataset_List = ['avenue_train']
 
 datasets = {}
 datasets['mytestvideo'] = {
-    #todo 지울것 : program test data set
+    # todo 지울것 : program test data set
     'path': DATASET_PATH + '/Test_JY/training_videos',
     'dataset': 'Test_JY',
     'type': 'train',
@@ -58,147 +56,187 @@ datasets['enter_test'] = {
     'name_format': '%02d.avi',
     'num_videos': 6
 }
+
+
 # =============================================================================
 # SET DATASET LIST
 # =============================================================================
+
+
+def make_dir(path):
+    # if there is no directory, make a directory.
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return
+
+
+def call_files(path, file_type):
+    # return file list of the given type in the given path.
+    allfiles = os.listdir(path)
+    imglist = [filename for filename in allfiles if filename[-(len(file_type[0])):] in file_type]
+    imglist.sort()
+    return imglist
+
 
 # =============================================================================
 # EXTRACT FRAMES FROM VIDEOS
 # =============================================================================
 
-# FFMPEG -> frame extraction & rescale & grayscale  & save to disk(png)
+
 def Video_to_image():
+    # FFMPEG -> frame extraction & rescale & grayscale  & save to disk(png)
     for dataset_name in dataset_List:
-        for video in range(1, datasets[dataset_name]['num_videos']+1):
-                if not os.path.exists(datasets[dataset_name]['path']+'/%02d' % video):
-                    os.makedirs(datasets[dataset_name]['path']+'/%02d' % video)
-                command = [FFMPEG_BIN,
-                           '-i', datasets[dataset_name]['path']+'/%02d.avi' % video,
-                           '-s', str(target_rows)+'x'+str(target_cols),
-                           '-pix_fmt', 'gray',
-                           datasets[dataset_name]['path']+'/%02d' % video+'/output_%05d.png']
-                sp.call(command)
+        for video in range(1, datasets[dataset_name]['num_videos'] + 1):
+            make_dir(datasets[dataset_name]['path'] + '/%02d' % video)
+            # make command.
+            command = [FFMPEG_BIN,
+                       '-i', datasets[dataset_name]['path'] + '/%02d.avi' % video,
+                       '-s', str(target_rows) + 'x' + str(target_cols),  # [rows x cols]
+                       '-pix_fmt', 'gray',  # make to grayscale
+                       datasets[dataset_name]['path'] + '/%02d' % video + '/output_%05d.png']
+            sp.call(command)  # call command
 
     print("================Video to image done==================")
-
     return
+
 
 # =============================================================================
 # PREPROCESS
 # =============================================================================
-#이미지를 열어서 갯수를 구하고, frame stride에 해당하는 이미지들을 복사한다. 그리고 그 이미지들의 개수를 센다.
-#데이터셋이 여러개일 경우가 있어 따로 저장해야한다.
-#todo 이거 없앨 수 있을 것 같다.
-
 def Sample_data_and_count():
+    # The number-of-images is obtained, and the images of each frame_stride-th copy are copied.
     global number_of_images_in_dataset_all
 
     for dataset_name in dataset_List:
-        for video in range(1, datasets[dataset_name]['num_videos']+1):
-            allfiles = os.listdir(datasets[dataset_name]['path']+'/%02d' % video)
-            imglist = [filename for filename in allfiles if filename[-4:] in [".png", ".PNG"]]
-            number_of_images_in_dataset_all += len(imglist)     # number of images for mean image.
+        for video in range(1, datasets[dataset_name]['num_videos'] + 1):
+            imglist = call_files(datasets[dataset_name]['path'] + '/%02d' % video, [".png", ",PNG"])
+            number_of_images_in_dataset_all += len(imglist)  # number of images for mean image.
 
-            sampled_image_path = datasets[dataset_name]['path'] + '/%02d' % video+'/sampled_image'
-            if not os.path.exists(sampled_image_path):
-                os.makedirs(sampled_image_path)
+            sampled_image_path = datasets[dataset_name]['path'] + '/%02d' % video + '/sampled_image'
+            make_dir(sampled_image_path)
 
-            for stride in range(1, len(imglist), frame_stride):
-                shutil.copy(datasets[dataset_name]['path'] + '/%02d' % video + "/" + imglist[stride], sampled_image_path)
-
-
-
+            for stride in range(0, len(imglist), frame_stride):
+                shutil.copy(datasets[dataset_name]['path'] + '/%02d' % video + "/" + imglist[stride],
+                            sampled_image_path)
     print("============Sample data and count done===============")
     return
 
+
 def Make_mean_image():
-
-
+    # make mean images.
     for dataset_name in dataset_List:
-        mean_image = numpy.zeros((target_rows, target_cols), numpy.float)  # mean image container
-        if(datasets[dataset_name]['type']=='train'):
-            for video in range(1, datasets[dataset_name]['num_videos']+1):
+        if (datasets[dataset_name]['type'] == 'train'):
+            mean_image = numpy.zeros((target_rows, target_cols), numpy.float)  # mean image container
+            for video in range(1, datasets[dataset_name]['num_videos'] + 1):
                 image_path = datasets[dataset_name]['path'] + '/%02d' % video
-                allfiles = os.listdir(image_path)
-                imglist = [filename for filename in allfiles if filename[-4:] in [".png", ".PNG"]]
+                imglist = call_files(image_path, [".png", ".PNG"])
 
                 for im in imglist:
-                    imarr = numpy.array(Image.open(image_path+'/'+im), dtype=numpy.float)
-                    imarr = imarr/255 #normalize
+                    imarr = numpy.array(Image.open(image_path + '/' + im), dtype=numpy.float)
                     mean_image = mean_image + imarr
-            mean_image = mean_image / number_of_images_in_dataset_all
-            numpy.save(os.path.join(DATASET_PATH + '/' + datasets[dataset_name]['dataset'] + '/mean_image'), mean_image)
-            Image.fromarray(mean_image*255).show()
-                #out.show()
+
+            mean_image = mean_image / number_of_images_in_dataset_all   # make mean image.
+            numpy.save(os.path.join(DATASET_PATH + '/' + datasets[dataset_name]['dataset'] + '/mean_image'),
+                       mean_image)  # save mean image .npy
+            #Image.fromarray(mean_image).show()  # make sure you make it right.
     print("===============Make mean image done==================")
     return
 
-def Generate_sample():
 
+def Make_Image_minus_mean(sampled_image_path, dataset_name):
+    imglist = call_files(sampled_image_path, [".png", ".PNG"])
+    num_of_images = len(imglist)
+    mean_image = numpy.load(DATASET_PATH + '/' + datasets[dataset_name]['dataset'] + '/mean_image.npy')  # load mean image
+    for im in imglist:
+        imarr = numpy.array(Image.open(sampled_image_path + '/' + im), dtype=numpy.float)
+
+        out = imarr - mean_image
+        out = out / 255
+        torch.save(out, sampled_image_path + '/' + im[:-4] + '.t7') #save t7 file (1x277x277 float)
+
+        # *this section causes overwrite sampled image(just for test)------------
+        out = numpy.abs(out)
+        out = out * 255
+        out = numpy.array(numpy.round(out), dtype=numpy.uint8)
+        Image.fromarray(out, mode="L").save(sampled_image_path + '/' + im)  # image visualize
+        # ------------------------------------------------------------------------
+    return num_of_images
+
+def Make_Image_minus_mean_Threshold(sampled_image_path, dataset_name,threshold_value):
+    imglist = call_files(sampled_image_path, [".png", ".PNG"])
+    num_of_images = len(imglist)
+    mean_image = numpy.load(DATASET_PATH + '/' + datasets[dataset_name]['dataset'] + '/mean_image.npy')  # load mean image
+    for im in imglist:
+        imarr = numpy.array(Image.open(sampled_image_path + '/' + im), dtype=numpy.float)
+
+        out = imarr - mean_image
+        out = numpy.where((numpy.abs(out) < threshold_value), 0, out)
+        out = out / 255
+        torch.save(out, sampled_image_path + '/' + im[:-4] + '.t7') #save t7 file (1x277x277 float)
+
+        # *this section causes overwrite sampled image(just for test)------------
+        out = numpy.abs(out)
+        out = out * 255
+        out = numpy.array(numpy.round(out), dtype=numpy.uint8)
+        Image.fromarray(out, mode="L").save(sampled_image_path + '/' + im)  # image visualize
+        # ------------------------------------------------------------------------
+    return num_of_images
+
+
+def Generate_InputData(preprocessing_type):
     for dataset_name in dataset_List:
-        mean_image = numpy.load(DATASET_PATH + '/' + datasets[dataset_name]['dataset'] + '/mean_image.npy')
-        Image.fromarray(mean_image*255).show()
-        print(mean_image)
+        # make sure you get it right
         if (datasets[dataset_name]['type'] == 'train'):
-            final_datasample_path = datasets[dataset_name]['path'] + '/finalDataset_train'
-            if not os.path.exists(final_datasample_path):
-                os.makedirs(final_datasample_path)
-        h = 0
+            Inputdata_path = datasets[dataset_name]['path'] + '/finalDataset_train'
+            make_dir(Inputdata_path)
 
-
+        savedImageNumber = 0  # it will be used for naming.
 
         for video in range(1, datasets[dataset_name]['num_videos'] + 1):
-            if(datasets[dataset_name]['type']=='test'):
-               if not os.path.exists(datasets[dataset_name]['path'] + '/testDataset_%02d' % video):
-                    os.makedirs(datasets[dataset_name]['path'] + '/testDataset_%02d' % video)
-
+            if (datasets[dataset_name]['type'] == 'test'):
+                make_dir(datasets[dataset_name]['path'] + '/testDataset_%02d' % video)
             sampled_image_path = datasets[dataset_name]['path'] + '/%02d' % video + '/sampled_image'
-            allfiles = os.listdir(sampled_image_path)
-            imglist = [filename for filename in allfiles if filename[-4:] in [".png", ".PNG"]]
-            num_of_images = len(imglist)
+            if preprocessing_type == 'mean image':
+                num_of_images = Make_Image_minus_mean(sampled_image_path,dataset_name)
+            elif preprocessing_type == 'mean image threshold':
+                num_of_images = Make_Image_minus_mean_Threshold(sampled_image_path, dataset_name,
+                                                                threshold_value=threshold_value)
 
 
-            for im in imglist:
-                imarr = numpy.array(Image.open(sampled_image_path + '/' + im), dtype=numpy.float)
+        # Create input data by grouping the created files.
+        imglist_f = call_files(sampled_image_path, [".t7", ".T7"])
+        imglist_f.sort()
+        start_frame = 0 #initialize start point
+        end_frame = start_frame + target_length
+        while (1):
+            if (end_frame > num_of_images):
+                break
 
-                #전처리 부분 함수화 하기 .
-                out = imarr/255 - mean_image
-                torch.save(out, sampled_image_path + '/' + im[:-4]+'.t7')
+            datatsr = [torch.load(sampled_image_path + '/' + imglist_f[k]) for k in range(start_frame, end_frame)]
+            #make inputdata set (target_Length x 277 x 277 float)
 
-                #-todo change sampled image(just for test)
-                out = numpy.abs(out)
-                out = out*255
-                out= numpy.array(numpy.round(out), dtype=numpy.uint8)
+            if (datasets[dataset_name]['type'] == 'train'):
+                torch.save(datatsr, Inputdata_path + '/%06d.t7' % savedImageNumber)
+            elif (datasets[dataset_name]['type'] == 'test'):
+                torch.save(datatsr,
+                           datasets[dataset_name][
+                               'path'] + '/finalDataset_%02d' % video + '/%06d.t7' % savedImageNumber)
+            savedImageNumber = savedImageNumber + 1
+            start_frame = start_frame + sample_stride
+            end_frame = end_frame + sample_stride
 
-                Image.fromarray(out, mode="L").save(sampled_image_path + '/' + im)#image visualize
-
-            allfiles = os.listdir(sampled_image_path)
-            imglist = [filename for filename in allfiles if filename[-3:] in [".t7", ".T7"]]
-            i = 1
-            j = i + target_length
-            while (1):
-                if (j > num_of_images):
-                    break
-                #datatsr= [numpy.array(Image.open(sampled_image_path + '/' + imglist[k]), dtype=numpy.float) for k in range(i, i + target_length)]
-                datatsr= [torch.load(sampled_image_path + '/' + imglist[k]) for k in range(i, i + target_length)]
-
-                if(datasets[dataset_name]['type']=='train'):
-                    torch.save(datatsr, final_datasample_path+'/%06d.t7' % h)
-                elif(datasets[dataset_name]['type']=='test'):
-                    torch.save(datatsr, datasets[dataset_name]['path'] + '/finalDataset_%02d' % video +'/%06d.t7' % h)
-                h = h+1
-                i = i+sample_stride
-                j = j+sample_stride
-    print("===============Generate sample done==================")
+    print("==============Generate inputdata done================")
     return
-#todo - 저장한 파일의 명세서 저장할 것. 크기, 프레임 등
+
+
+# todo - 저장한 파일의 명세서 저장할 것. 크기, 프레임 등
 # =============================================================================
 # GENERATE SAMPLES WITH IMAGES
 # =============================================================================
 
 print("Dataset List : avenue_train,avenue_test,enter_train,enter_test \n you can select multiple dataset. \n "
       "if you want stop enter name, plz enter 'doen' \n")
-#todo-원하는 데이터셋 선택
+# todo-원하는 데이터셋 입력
 '''
 while(1):
     dataset_name = input("plz enter dataset name : ")
@@ -210,11 +248,16 @@ while(1):
     dataset_List.append(dataset_name)
 '''
 
+
 Video_to_image()
 Sample_data_and_count()
+# ---
 Make_mean_image()
-Generate_sample()
+threshold_value = 15
+Generate_InputData('mean image')
+#'mean image' 'mean image threshold'
+# ---
 
 
-#todo-이미지 불러오고 저장하는 과정을 최대한 줄이자.
+# todo-이미지 불러오고 저장하는 과정을 최대한 줄이자.
 #
