ab6eba60bb8107d9dd881c81bc8c6a47a767f9e5
neohanju <neohanju@gmail.com>
2017-04-20 17:54:52 +0900

    modify sample generation code (=naming, multiple stride input for each dataset)

--- data_generation/generate_sample.py
+++ data_generation/generate_sample.py
@@ -6,24 +6,24 @@ import numpy as np
 import glob
 
 FFMPEG_BIN = "ffmpeg"
-DATASET_BASE_PATH = '/mnt/fastdataset/Datasets'
-
-number_of_images_in_dataset_all = 0
+DATASET_BASE_PATH = '/home/mlpa/Workspace/dataset/nips'
 
 target_rows = 227
 target_cols = 227
 target_length = 10
 
-frame_stride = 1
-sample_stride = 2
+# frame strides
+train_frame_strides = [1, 2, 3]
+test_frame_stride = 1
 
-train_frame_stride = [1, 2, 3]
+# sample strides
 train_sample_stride = 2
+test_sample_stride = 5
 
 # target_datasets = ['avenue_train', 'avenue_test',
 #                    'enter_train', 'enter_test',
 #                    'exit_train', 'exit_test']
-target_datasets = ['avenue_train','enter_train','exit_train']
+target_datasets = ['avenue_train', 'avenue_test']
 
 datasets = dict(
     avenue_train=dict(
@@ -32,7 +32,7 @@ datasets = dict(
         type='train',
         name_format='%02d.avi',
         num_videos=16,
-        frame_stride=train_frame_stride,
+        frame_stride=train_frame_strides,
         sample_stride=train_sample_stride
     ),
     avenue_test=dict(
@@ -41,8 +41,8 @@ datasets = dict(
         type='test',
         name_format='%02d.avi',
         num_videos=21,
-        frame_stride=[1],
-        sample_stride=5
+        frame_stride=test_frame_stride,
+        sample_stride=test_sample_stride
     ),
     enter_train=dict(
         path=os.path.join(DATASET_BASE_PATH, 'enter'),
@@ -50,7 +50,7 @@ datasets = dict(
         type='train',
         name_format='%02d.avi',
         num_videos=1,
-        frame_stride=train_frame_stride,
+        frame_stride=train_frame_strides,
         sample_stride=train_sample_stride
     ),
     enter_test=dict(
@@ -59,8 +59,8 @@ datasets = dict(
         type='test',
         name_format='%02d.avi',
         num_videos=6,
-        frame_stride=[1],
-        sample_stride=5
+        frame_stride=test_frame_stride,
+        sample_stride=test_sample_stride
     ),
     exit_train=dict(
         path=os.path.join(DATASET_BASE_PATH, 'exit'),
@@ -68,7 +68,7 @@ datasets = dict(
         type='train',
         name_format='%02d.avi',
         num_videos=1,
-        frame_stride=train_frame_stride,
+        frame_stride=train_frame_strides,
         sample_stride=train_sample_stride
     ),
     exit_test=dict(
@@ -77,8 +77,8 @@ datasets = dict(
         type='test',
         name_format='%02d.avi',
         num_videos=4,
-        frame_stride=[1],
-        sample_stride=5
+        frame_stride=test_frame_stride,
+        sample_stride=test_sample_stride
     )
 )
 
@@ -163,7 +163,7 @@ def get_mean_image():
 # =============================================================================
 # GENERATE SAMPLES
 # =============================================================================
-def generate_samples(centering=True):
+def generate_samples(centering=False):
 
     if centering:
         numpy_dtype = np.float32
@@ -172,82 +172,97 @@ def generate_samples(centering=True):
 
     # sample data container
     sample_data = np.zeros((target_length, target_rows, target_cols), dtype=numpy_dtype)
+
     for i, name in enumerate(target_datasets):
+
         print('Generate samples with "%s" dataset ... [%d/%d]' % (name, i+1, len(target_datasets)))
+
+        sample_stride = datasets[name]['sample_stride']
 
         # output folder
         make_dir(os.path.join(datasets[name]['path'], datasets[name]['type']))
-        # load mean image
 
+        # load mean image
         mean_image = np.load(os.path.join(datasets[name]['path'], 'mean_image.npy'))
 
         # loop to generate samples
-        sample_count = 0
-        for video in range(1, datasets[name]['num_videos'] + 1):
+        total_sample_count = 0
 
-            # get paths of target images
-            image_folder = os.path.join(datasets[name]['path'], '%sing_videos' % datasets[name]['type'],
-                                        os.path.splitext(datasets[name]['name_format'] % video)[0])
-            image_paths = get_file_paths(image_folder, ['png', 'PNG'])
-            target_image_paths = image_paths[0::frame_stride]
-            num_frames = len(target_image_paths)
+        frame_strides = [datasets[name]['frame_stride']] \
+            if isinstance(datasets[name]['frame_stride'], int) else datasets[name]['frame_stride']
 
-            # read target images and preprocess them
-            sys.stdout.write('\tAt "%s"... [0/%d]' % (datasets[name]['name_format'] % video, num_frames))
-            read_pos = 0
-            sample_count_wrt_video = 0
-            for start_pos in range(0, num_frames, sample_stride):
-                sys.stdout.write('\r\tAt "%s"... [%d/%d]'
-                                 % (datasets[name]['name_format'] % video, start_pos, num_frames))
+        for frame_stride in frame_strides:
+            stride_sample_count = 0
+            for video in range(1, datasets[name]['num_videos'] + 1):
 
-                # check end-of-processing
-                if start_pos + target_length > num_frames:
-                    break
+                # print('  with frame stride %d and sample stride %d' % (frame_stride, sample_stride))
+                # get paths of target images
+                image_folder = os.path.join(datasets[name]['path'], '%sing_videos' % datasets[name]['type'],
+                                            os.path.splitext(datasets[name]['name_format'] % video)[0])
+                image_paths = get_file_paths(image_folder, ['png', 'PNG'])
+                target_image_paths = image_paths[0::frame_stride]
+                num_frames = len(target_image_paths)
 
-                # reallocate already read images
-                sample_data = np.roll(sample_data, sample_stride, axis=0)
+                # read target images and preprocess them
+                sys.stdout.write('\t"%s" with frame stride %d and sample stride %d ... [0/%d]'
+                                 % (datasets[name]['name_format'] % video, frame_stride, sample_stride, num_frames))
+                read_pos = 0
+                sample_count_wrt_video = 0
+                for start_pos in range(0, num_frames, sample_stride):
+                    sys.stdout.write('\r\tAt "%s" with frame stride %d and sample stride %d ... [%d/%d]'
+                                     % (datasets[name]['name_format'] % video, frame_stride, sample_stride, start_pos,
+                                        num_frames))
 
-                # read and preprocess only unread images
-                reading_images = target_image_paths[read_pos:start_pos+target_length]
-                for j, path in enumerate(reading_images):
-                    image_data = np.array(Image.open(path), dtype=numpy_dtype)
+                    # check end-of-processing
+                    if start_pos + target_length > num_frames:
+                        break
 
-                    # preprocessing
-                    if centering:
-                        image_data -= mean_image
-                        image_data /= 255.0
+                    # reallocate already read images
+                    sample_data = np.roll(sample_data, sample_stride, axis=0)
 
-                    # insert to sample container
-                    sample_data[read_pos-start_pos+j] = image_data
+                    # read and preprocess only unread images
+                    reading_images = target_image_paths[read_pos:start_pos+target_length]
+                    for j, path in enumerate(reading_images):
+                        image_data = np.array(Image.open(path), dtype=numpy_dtype)
 
-                # different file name format and index among set type
-                file_name_format = 'fs_%d_stride_%d_%06d'
-                file_name_index = sample_count
-                if datasets[name]['type'] is 'test':
-                    file_name_format = 'video_%02d' % video + '_%06d'
-                    file_name_index = sample_count_wrt_video
-                save_file_path = os.path.join(datasets[name]['path'], datasets[name]['type'],
-                                              file_name_format % (frame_stride,sample_stride,file_name_index))
+                        # preprocessing
+                        if centering:
+                            image_data -= mean_image
+                            image_data /= 255.0
 
-                np.save(save_file_path, sample_data)
+                        # insert to sample container
+                        sample_data[read_pos-start_pos+j] = image_data
 
-                sample_count += 1
-                sample_count_wrt_video += 1
-                read_pos = start_pos + target_length
+                    # different file name format and index among set type
+                    file_name_format = name + '_frame_interval_%d_stride_%d_%06d'
+                    file_name_index = stride_sample_count
+                    if datasets[name]['type'] is 'test':
+                        file_name_format = name + '_video_%02d' % video + '_frame_interval_%d_stride_%d_%06d'
+                        file_name_index = sample_count_wrt_video
+                    save_file_path = os.path.join(datasets[name]['path'], datasets[name]['type'],
+                                                  file_name_format % (frame_stride, sample_stride, file_name_index))
 
-            print('\r\tAt "' + datasets[name]['name_format'] % video + '"... done!')
-        print('%d samples are generated.' % sample_count)
+                    np.save(save_file_path, sample_data)
+
+                    stride_sample_count += 1
+                    sample_count_wrt_video += 1
+                    read_pos = start_pos + target_length
+                print('\r\tAt "%s" with frame stride %d and sample stride %d is done!'
+                      % (datasets[name]['name_format'] % video, frame_stride, sample_stride))
+
+            total_sample_count += stride_sample_count
+
+        print('%d samples are generated.' % total_sample_count)
 
 
 # todo - 저장한 파일의 명세서 저장할 것. 크기, 프레임 등
 # =============================================================================
 # MAIN PROCEDURE
 # =============================================================================
-extract_video_frames()
-get_mean_image()
-for _frame_stride in train_frame_stride:
-    frame_stride = _frame_stride
-    generate_samples(centering=False)
+# extract_video_frames()
+# get_mean_image()
+generate_samples()
+
 
 # ()()
 # ('')HAANJU.YOO
